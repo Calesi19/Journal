0fa2c7ebf0d3008a590ba8eacd0b61e21520034f
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-12-13 22:02:16 -0600

    feat: landing page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,86 @@
+import { Button } from "@nextui-org/react";
+
+const lockSvg = (
+  <svg fill="#ffffff" height="32px" width="32px" version="1.1" id="Layer_1" viewBox="0 0 330 330">
+    <g id="XMLID_509_">
+      <path id="XMLID_510_" d="M65,330h200c8.284,0,15-6.716,15-15V145c0-8.284-6.716-15-15-15h-15V85c0-46.869-38.131-85-85-85
+		S80,38.131,80,85v45H65c-8.284,0-15,6.716-15,15v170C50,323.284,56.716,330,65,330z M180,234.986V255c0,8.284-6.716,15-15,15
+		s-15-6.716-15-15v-20.014c-6.068-4.565-10-11.824-10-19.986c0-13.785,11.215-25,25-25s25,11.215,25,25
+		C190,223.162,186.068,230.421,180,234.986z M110,85c0-30.327,24.673-55,55-55s55,24.673,55,55v45H110V85z"/>
+    </g>
+  </svg>
+)
+
 export default function Page() {
   return (
     <div>
-      <h1>Page</h1>
+      <header className="absolute inset-x-0 top-0 z-50">
+        <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
+          <div className="flex lg:flex-1">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="text-3xl font-semibold text-gray-900 text-white">
+                {lockSvg}
+              </span>
+            </a>
+          </div>
+        </nav>
+      </header>
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        >
+          <div
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#0070f0] to-[#1B8ED6] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          />
+        </div>
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              Built with React and Go.{' '}
+              <a href="https://github.com/Calesi19/Journal" className="font-semibold text-[#0070f0]">
+                <span aria-hidden="true" className="absolute inset-0" />
+                See code <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+          <div className="text-center">
+            <h1 className="text-balance text-5xl tracking-tight text-white sm:text-7xl font-black">
+              journaling made simple.
+            </h1>
+            <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+              A secure journaling app that allows users to create, edit, and organize private entries with ease. It offers personalized prompts and summaries to enhance the writing experience.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+
+              <a href="/create-account">
+                <Button color="primary">
+                  Create Account
+                </Button>
+              </a>
+              <a href="/login" className="text-sm/6 font-semibold text-white">
+                Log In <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        >
+          <div
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#0070f0] to-[#1B8ED6] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          />
+        </div>
+      </div>
     </div>
   );
 }
