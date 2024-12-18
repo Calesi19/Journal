77c5bb2e5e987088e29971cb7e1b62517a87695c
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-12-18 14:42:49 -0600

    fix: protected routes

--- backend/JournalApiGo/internal/routers/routers.go
+++ backend/JournalApiGo/internal/routers/routers.go
@@ -17,14 +17,14 @@ func RegisterRoutes(e *echo.Echo) {
 
 	// account routes
 	e.POST("/accounts", accounts.CreateAccountHandler(database.DB))
-	e.DELETE("/accounts", accounts.DeleteAccountHandler(database.DB))
 
 	// posts routes
-	postsGroup := e.Group("/posts", middleware.AuthMiddleware)
-	postsGroup.GET("", posts.GetPostsHandler(database.DB))
-	postsGroup.POST("", posts.CreatePostHandler(database.DB))
-	postsGroup.DELETE("/:id", posts.DeletePostHandler(database.DB))
-	postsGroup.PUT("/:id", posts.UpdatePostHandler(database.DB))
+	protected := e.Group("/", middleware.AuthMiddleware)
+	protected.DELETE("/accounts/:id", accounts.DeleteAccountHandler(database.DB))
+	protected.GET("/posts", posts.GetPostsHandler(database.DB))
+	protected.POST("/posts", posts.CreatePostHandler(database.DB))
+	protected.DELETE("/posts/:id", posts.DeletePostHandler(database.DB))
+	protected.PUT("/posts/:id", posts.UpdatePostHandler(database.DB))
 
 	// health check
 	e.GET("/health", func(c echo.Context) error {
