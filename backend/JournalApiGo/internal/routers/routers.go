8f788bcc8185fc94453627873ede519edd74f069
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-12-24 11:33:51 -0400

    fix: health check package

--- backend/JournalApiGo/internal/routers/routers.go
+++ backend/JournalApiGo/internal/routers/routers.go
@@ -4,6 +4,7 @@ import (
 	"journal-api/internal/database"
 	"journal-api/internal/handlers/accounts"
 	"journal-api/internal/handlers/auth"
+	"journal-api/internal/handlers/health"
 	"journal-api/internal/handlers/posts"
 	"journal-api/internal/middleware"
 
@@ -11,35 +12,18 @@ import (
 )
 
 func RegisterRoutes(e *echo.Echo) {
-	// auth routes
+	e.GET("/health", health.HealthCheckHandler(database.DB))
+
 	e.POST("/login", auth.LoginHandler(database.DB))
 	e.POST("/refresh-token", auth.RefreshTokenHandler())
 
-	// account routes
 	e.POST("/accounts", accounts.CreateAccountHandler(database.DB))
 
-	// posts routes
+	// protected routes
 	protected := e.Group("/", middleware.AuthMiddleware)
 	protected.DELETE("/accounts/:id", accounts.DeleteAccountHandler(database.DB))
 	protected.GET("/posts", posts.GetPostsHandler(database.DB))
 	protected.POST("/posts", posts.CreatePostHandler(database.DB))
 	protected.DELETE("/posts/:id", posts.DeletePostHandler(database.DB))
 	protected.PUT("/posts/:id", posts.UpdatePostHandler(database.DB))
-
-	// health check
-	e.GET("/health", func(c echo.Context) error {
-		err := database.DB.Ping()
-		if err != nil {
-			return c.JSON(500, map[string]string{
-				"status":   "error",
-				"database": "error",
-			})
-		}
-
-		response := map[string]string{
-			"status":   "ok",
-			"database": "ok",
-		}
-		return c.JSON(200, response)
-	})
 }
